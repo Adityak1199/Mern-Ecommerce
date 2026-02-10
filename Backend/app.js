39f8f4025fe9653b11ad1a91b82976da31b32eb2
Adityak1199 <adityak.ug23.cs@nitp.ac.in>
2026-02-10 22:00:29 +0530

    My Ecommerce Project

---
+++ Backend/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import product from "./routes/productRoutes.js";
+import user from "./routes/userRoutes.js";
+import order from "./routes/orderRoutes.js";
+import payment from "./routes/paymentRoutes.js";
+import errorHandleMiddleware from "./middleware/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import path from "path";
+import { fileURLToPath } from "url";
+const app = express();
+import dotenv from "dotenv";
+
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
+app.use(express.urlencoded({extended:true}));
+// route
+app.use("/api/v1",product);
+app.use("/api/v1",user);
+app.use("/api/v1",order);
+app.use("/api/v1",payment);
+
+// serve static file
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+});
+
+
+
+app.use(errorHandleMiddleware);
+if(process.env.NODE_ENV !=='PRODUCTION'){
+dotenv.config({path:"backend/config/config.env"});
+}
+
+
+
+export default app;
